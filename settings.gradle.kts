bdcde4f8c28b77d375b46863fb0a120c8c026694
Steven McAdams <steven.mcadams@datasite.com>
2020-11-05 09:02:36 -0600

    sort lines

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 rootProject.name = "datasite-poc-garden"
 
 import(":auditing:common")
+import(":iot:common")
+import(":iot:gateway")
+import(":iot:simulator")
 import(":processor")
 import(":reporting:common")
 import(":reporting:service")
 import(":reporting:ui")
 import(":service")
-import(":iot:gateway")
-import(":iot:simulator")
-import(":iot:common")
 
 fun import(name: String) {
     val projectName = name.replace(":", "-").removePrefix("-")
