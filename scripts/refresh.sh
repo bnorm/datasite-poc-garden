121f6e56ee87c388c9284efab82935a2022fb972
Steven McAdams <steven.mcadams@datasite.com>
2020-11-06 08:20:34 -0600

    add refresh script

---
+++ scripts/refresh.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "> destroying docker-compose..."
+docker-compose rm -f -s
+
+docker-compose up -d
+
+sleep 5
+echo "initiating mongo cluster..."
+
+docker-compose exec mongo /bin/bash -c 'mongo localhost:27017/datasite-poc <<-EOF
+    rs.initiate({
+        _id: "rs0",
+        "version" : 1,
+        members: [
+            { _id: 0, host: "localhost:27017" }
+        ]
+    });
+EOF'
+
+sleep 5
+echo "initiating debezium connector"
+
+curl -i -X POST \
+  -H "Accept:application/json" \
+  -H  "Content-Type:application/json" \
+  http://localhost:8083/connectors/ \
+  -d '{
+  "name": "mongodb_connector",
+  "config": {
+    "connector.class" : "io.debezium.connector.mongodb.MongoDbConnector",
+    "tasks.max" : "1",
+    "mongodb.hosts" : "rs0/mongo:27017",
+    "mongodb.name" : "mongo",
+    "mongodb.members.auto.discover" : false,
+    "collection.whitelist" : "datasite-poc.*",
+    "provide.transaction.metadata" : true,
+    "database.history.kafka.bootstrap.servers" : "kafka:9092"
+  }
+}'
+
+
